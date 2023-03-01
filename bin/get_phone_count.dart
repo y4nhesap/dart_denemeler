7ae83e4b198a637c92a878ad32a608e014ffe518
y4nhesap <dumendenhesap24@gmail.com>
2023-03-01 10:00:57 +0300

    first commit

---
+++ bin/get_phone_count.dart
@@ -0,0 +1,27 @@
+import 'dart:math';
+
+class GetCountryPhoneCount {
+  GetCountryPhoneCount() {
+    print(getCountryPhoneCount(14).toString());
+  }
+}
+
+//get number of phones starts with parameter
+int getCountryPhoneCount(int countryCode) {
+  List<String> list = randomPhoneNumberGenerator();
+  print(list);
+  return list
+      .where((element) => element.startsWith("+$countryCode "))
+      .toList()
+      .length;
+}
+
+//random 100 number generator
+List<String> randomPhoneNumberGenerator() {
+  List<String> list = [];
+  for (int i = 0; i < 100; i++) {
+    list.add(
+        "+${Random().nextInt(100)} ${Random().nextInt(500000000) + 5000000000}"); //5000000000 to 5500000000
+  }
+  return list;
+}
---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'get_phone_count.dart';
+
+void main() {
+  GetCountryPhoneCount();
+}
